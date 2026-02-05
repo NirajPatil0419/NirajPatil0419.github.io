814d4e4a22f32ee9597e182df7b8f39b963d5814
NirajPatil0419 <niraj.patil412@gmail.com>
2026-02-05 14:52:34 +0530

    updated interface

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 const yesBtn = document.getElementById("yesBtn");
 const noBtn = document.getElementById("noBtn");
-const message = document.getElementById("message");
+const message = document.getElementById("loveMessage");
+const container = document.querySelector(".buttons");
 
-yesBtn.onclick = () => {
-  message.style.display = "block";
+yesBtn.addEventListener("click", () => {
+  message.classList.remove("hidden");
+  yesBtn.style.display = "none";
   noBtn.style.display = "none";
-  yesBtn.innerText = "❤️ Thank You!";
-};
-noBtn.onclick = () => alert("Are you sure? 😢");
+});
+
+function moveNoButton() {
+  const containerRect = container.getBoundingClientRect();
+  const btnRect = noBtn.getBoundingClientRect();
+
+  const maxX = containerRect.width - btnRect.width;
+  const maxY = containerRect.height - btnRect.height;
+
+  const x = Math.random() * maxX;
+  const y = Math.random() * maxY;
+
+  noBtn.style.left = `${x}px`;
+  noBtn.style.top = `${y}px`;
+}
+
+noBtn.addEventListener("mouseover", moveNoButton);
+noBtn.addEventListener("click", moveNoButton); // for mobile 😈
